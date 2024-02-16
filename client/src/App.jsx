daf6ac0a5d625721bf5505823f6ac5c8bf1c3856
Riya Patro <riyapatro18@gmail.com>
2024-02-16 20:58:15 +0530

    crate pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import {BrowserRouter , Routes, Route} from "react-router-dom"
+
+// Importing pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
 
 const App = () => {
   return (
-    <div className='bg-green text-yellow-600 text-8xl font-semibold'>
-      hellooo
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/signin' element={<Signin/>} />
+    <Route path='/signout' element={<Signout/>} />
+    <Route path='/dashboard' element={<Dashboard/>} />
+    <Route path='/projects' element={<Projects/>} />
+   </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      aboute
+    </div>
+  )
+}
+
+export default About
