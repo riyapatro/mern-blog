110bfdcba8c6b4dacfb327ddf03800b62cb9ed5f
Riya Patro <riyapatro18@gmail.com>
2024-02-16 20:19:45 +0530

    setup for blog project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-green text-yellow-600 text-8xl font-semibold'>
+      hellooo
+    </div>
+  )
+}
+
+export default App
