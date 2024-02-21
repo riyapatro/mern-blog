b931f59b06e2c3ac87a2c4ceafd102b5b83597d1
Riya Patro <riyapatro18@gmail.com>
2024-02-22 01:14:46 +0530

    Add fuctionality to signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import {BrowserRouter , Routes, Route} from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
 import Signin from './pages/Signin'
-import Signout from './pages/Signout'
+import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
@@ -19,7 +19,7 @@ const App = () => {
     <Route path='/' element={<Home/>} />
     <Route path='/about' element={<About/>} />
     <Route path='/signin' element={<Signin/>} />
-    <Route path='/signout' element={<Signout/>} />
+    <Route path='/signup' element={<Signup/>} />
     <Route path='/dashboard' element={<Dashboard/>} />
     <Route path='/projects' element={<Projects/>} />
    </Routes>
